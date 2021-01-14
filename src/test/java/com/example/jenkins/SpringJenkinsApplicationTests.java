98c65e3a4c8f87c0eaa88a2b19ebcbd759f5cf54
LCCFigher <chaudharybsc13@gmail.com>
2021-01-15 03:26:17 +0530

    spring jenkins 2 commit

--- src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
 	@Test
 	void contextLoads() {
+		logger.info("Test case Executed.....");
 	}
 
 }
